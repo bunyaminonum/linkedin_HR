5716b5c4cafbe8f5a88138a57690b5cfd558e89b
bunyamin.onum <bunyamin.onum@bentego.com>
2023-01-23 15:58:01 +0300

    added education info

--- info.py
+++ info.py
@@ -31,7 +31,8 @@ class Person:
     # def setLink(self, link:dict):
     #     self.infoList.update(link)
 
-
+    def setEducation(self, edu:dict):
+        self.infoList.update(edu)
 
 class GetInfo(GetProfileLinks):
     GRADUATE_XPATH = '/html/body/div[5]/div[3]/div/div/div/div[2]/div/div/main/section[4]/div[3]/ul/li/div/div[2]/div/a/span[2]/span[1]'
@@ -39,6 +40,8 @@ class GetInfo(GetProfileLinks):
     # LANGUAGE_PATH = '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[8]/div[3]/ul/li/div/div[2]/div'
     FOLLOWERS = 'pvs-header__subtitle'
     NUM_CONNECTION_PATH2 = '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/ul/li/a/span/span'
+    # BEST_SKILLS_CLASS = 'pvs-list__outer-container'
+    BEST_SKILLS_PATH = '/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[4]/div[3]'
     def __init__(self, email:str, password:str, pageNum = 1):
         super().__init__(email, password, pageNum)
         self.db = MDB()
@@ -74,7 +77,7 @@ class GetInfo(GetProfileLinks):
                     src = self.driver.page_source
 
                     soup = BeautifulSoup(src, 'lxml')
-                    intro = soup.find('div', {'class': 'pv-text-details__left-panel'})
+                    intro = soup.find('div', {'class': 'pv-educationInfo-details__left-panel'})
                     # self.person.setLink({'link':link})
                     try:
                         name_loc = intro.find("h1")
@@ -84,7 +87,7 @@ class GetInfo(GetProfileLinks):
                         name_loc = None
 
                     try:
-                        works_at_loc = intro.find("div", {'class': 'text-body-medium'})
+                        works_at_loc = intro.find("div", {'class': 'educationInfo-body-medium'})
                         works_at = works_at_loc.get_text().strip()
                         self.person.setWorksAt({'works at' : works_at})
                     except:
@@ -98,21 +101,29 @@ class GetInfo(GetProfileLinks):
 
                     try:
                         follow = self.driver.find_element_by_class_name(self.FOLLOWERS).text
-                        parse = float(str(follow).split()[0].strip().replace(',','.'))
-                        self.person.setConnectionNum({'num_connection':parse})
+                        follow = float(str(follow).split()[0].strip().replace(',','.'))
+                        self.person.setConnectionNum({'num followers':follow})
                     except NoSuchElementException:
                         follow = None
 
+                    education = self.driver.find_element_by_xpath(self.BEST_SKILLS_PATH)
+                    edu = education.find_elements_by_tag_name("li")
+                    edulist = list
+                    for item in edu:
+                        educationInfo = item.text
+                        self.person.setEducation({'education':educationInfo})
+                        
                     # print(self.person.infoList)
                     self.db.collection.insert_one(self.person.infoList)
+
 
                     # self.personInfo.append(self.person.infoList)
                     # print(self.personInfo)
                     print("Name -->", name,
                           "\nWorks At -->", works_at,
                           "\nLocation -->", location,
-                          "\n num connection", follow,
-                          "\nparse_numconnection", parse)
+                          "\n num followers", follow,
+                          "\neducation", educationInfo)
                 else:
                     continue
                       # "\nnumber of connection -->", num_connection)
